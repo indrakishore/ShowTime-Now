406276b61f97f6f13fb70370e4844eb617e71ec0
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2024-08-07 03:14:22 +0530

    Initial commit

---
+++ book-my-show/src/main/java/com/indra/book_my_show/Models/UserEntity.java
@@ -0,0 +1,23 @@
+package com.indra.book_my_show.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+
+}
